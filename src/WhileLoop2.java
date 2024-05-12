e74e2ccf1d7114b87c5b49693411799e365ff46e
Sanidu Perera <pererasanidu1234@gmail.com>
2024-05-12 19:37:13 +0900

    ReCorrect commit

---
+++ src/WhileLoop2.java
@@ -0,0 +1,14 @@
+public class WhileLoop2 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int i = 1;
+        while(i <= 10){
+            sum = sum +i;
+            i++;
+
+            System.out.println(sum);
+        }
+    }
+}
