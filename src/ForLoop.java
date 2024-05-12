2160fcfbd737823475d2d2a801514c7e6d2cb457
Sanidu Perera <pererasanidu1234@gmail.com>
2024-05-12 19:00:28 +0900

    Second commit

---
+++ src/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+        int number = 2;
+        for(int i = 1; i<=10; i++) {
+            System.out.println(number + " * " + i + " = " + (number*i));
+        }
+    }
+
+}
+
---
+++ src/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+        for(i = 1; i<=10;) {
+            while (i <= 10) {
+                sum+=i;
+                i++;
+
+                System.out.println(sum);
+            }
+    }   }
+}
