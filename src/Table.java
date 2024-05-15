7829c83cade3d0c7c0712214957cc58f14afb69c
Sanidu Perera <pererasanidu1234@gmail.com>
2024-05-15 17:26:51 +0900

    ForLoop another ex

---
+++ src/Table.java
@@ -0,0 +1,17 @@
+public class Table {
+
+
+    public static void main(String[] args) {
+
+        // Table of 8
+        // 8 * 1 = 8
+        // 8 * 2 = 16
+
+        int number = 8;
+        for(int i = 1; i<=10; i++) {
+            System.out.println(number + " * " + i + " = " + (number*i));
+        }
+    }
+
+}
+
